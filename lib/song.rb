ac589e540aee05ca1035d23b100104c2dd969776
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-31 14:44:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = [  ]
+  @@genres = [  ]
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def new(name, artist, genre)
+    @name
+    @artists
+    @genre
+  end
+  
+  def count(arr)
+   @@count = arr.length
+  end
+  
+end
